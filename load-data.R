3b88e072cbdfef754de62bd86eed82e39c69f060
jessilyn <jessilyn@stanford.edu>
2017-12-19 16:29:20 -0800

    removed normalized and commented out lines
    still seems to be a lot of unnecessary code in here for merging data frames for example. to make this code easy to follow, should remove code that is not contributing to final product

--- load-data.R
+++ load-data.R
@@ -9,7 +9,10 @@
 #install.packages("psych")
 require(psych)
 
-#### READ DATA ####
+dir = "/Users/jessilyn/Desktop/framework_paper/weartals/"
+
+
+#### READ DATA ####'
 vitals <- read.csv(
   paste0(dir,"vitals.csv"),
   header=TRUE,sep=',',stringsAsFactors=FALSE)
@@ -44,22 +47,19 @@ vitals[,c("Pulse","Temp")] <- apply(
   function(x) as.numeric(as.character(x)))
 
 #Describe
-describe(vitals$Pulse)
-describe(vitals$Temp)
+# describe(vitals$Pulse)
+# describe(vitals$Temp)
+# 
+# #Plot hist
+# hist(vitals$Pulse)
+# hist(vitals$Temp)
 
-#Plot hist
-hist(vitals$Pulse)
-hist(vitals$Temp)
-
-#Transform to normal (in this case sqrt pulse; winsor Temp)
-describe(sqrt(vitals$Pulse))
-vitals$Pulse <- sqrt(vitals$Pulse)
-describe(winsor(vitals$Temp, trim = 0.05))
-vitals$Temp <- winsor(vitals$Temp, trim = 0.05)
-
-#Recheck normalization
-describe(vitals$Pulse)
-describe(vitals$Temp)
+## REMOVED THIS FROM SCRIPT ##
+# #Transform to normal (in this case sqrt pulse; winsor Temp)
+# describe(sqrt(vitals$Pulse))
+# vitals$Pulse <- sqrt(vitals$Pulse)
+# describe(winsor(vitals$Temp, trim = 0.05))
+# vitals$Temp <- winsor(vitals$Temp, trim = 0.05)
 
 #### PREP LABS DATA ####
 
@@ -79,7 +79,6 @@ allClin <- c("A1C","AG","ALB","ALCRU","ALKP","ALT","AST","BASO",
              "MCH","MCHC","MCV","MONO","MONOAB","NA.","NEUT",
              "NEUTAB","NHDL","PLT","RBC","RDW","TBIL","TGL","TP",
              "UALB","UALBCR","WBC")
-#Leave out UALBCRINTP, as it is more categorical than continuous.
 
 for(i in 1:length(allClin)){ #this removes non-numeric characters
   cache <- labs[,c(allClin[i])]
@@ -90,141 +89,6 @@ for(i in 1:length(allClin)){ #this removes non-numeric characters
 labs[,c(allClin)] <- apply(
   labs[,c(allClin)], 2,
   function(x) as.numeric(as.character(x)))
-
-#Describe
-nonNormal <- list()
-for(i in 1:length(allClin)){
-  if(describe(
-    labs[,allClin[i]])$skew>1 | describe(
-      labs[,allClin[i]])$kurtosis>1){
-    print(allClin[i])
-    print(describe(labs[,allClin[i]]))
-    nonNormal[[i]] <- allClin[i]
-  }
-}
-
-#Transform to normal 
-nonNormal <- unlist(nonNormal)
-
-describe(labs[,nonNormal[1]])
-describe(sqrt(labs[,nonNormal[1]]))
-labs[,nonNormal[1]] <- sqrt(labs[,nonNormal[1]])
-
-describe(labs[,nonNormal[2]])
-describe(log10(labs[,nonNormal[2]]))
-labs[,nonNormal[2]] <- log10(labs[,nonNormal[2]])
-
-describe(labs[,nonNormal[3]])
-describe(log10(labs[,nonNormal[3]]))
-labs[,nonNormal[3]] <- log10(labs[,nonNormal[3]])
-
-describe(labs[,nonNormal[4]])
-describe(log10(labs[,nonNormal[4]]))
-labs[,nonNormal[4]] <- log10(labs[,nonNormal[4]])
-
-describe(labs[,nonNormal[5]]+1)
-describe(log10(labs[,nonNormal[5]]+1))
-labs[,nonNormal[5]] <- log10(labs[,nonNormal[5]]+1)
-
-describe(labs[,nonNormal[6]])
-describe(sqrt(labs[,nonNormal[6]]))
-labs[,nonNormal[6]] <- sqrt(labs[,nonNormal[6]])
-
-describe(labs[,nonNormal[7]])
-describe(sqrt(labs[,nonNormal[7]]))
-labs[,nonNormal[7]] <- sqrt(labs[,nonNormal[7]])
-
-describe(labs[,nonNormal[8]])
-describe(log10(labs[,nonNormal[8]]))
-labs[,nonNormal[8]] <- log10(labs[,nonNormal[8]])
-
-describe(labs[,nonNormal[9]])
-describe(log10(labs[,nonNormal[9]]+1))
-labs[,nonNormal[9]] <- log10(labs[,nonNormal[9]]+1)
-
-describe(labs[,nonNormal[10]])
-describe(log10(labs[,nonNormal[10]]+1))
-labs[,nonNormal[10]] <- log10(labs[,nonNormal[10]]+1)
-
-describe(labs[,nonNormal[11]])
-describe(log10(labs[,nonNormal[11]]))
-labs[,nonNormal[11]] <- log10(labs[,nonNormal[11]])
-
-describe(labs[,nonNormal[12]])
-describe(log10(labs[,nonNormal[12]]))
-labs[,nonNormal[12]] <- log10(labs[,nonNormal[12]])
-
-describe(labs[,nonNormal[13]])
-describe(log10(labs[,nonNormal[13]]))
-labs[,nonNormal[13]] <- log10(labs[,nonNormal[13]])
-
-describe(labs[,nonNormal[14]])
-describe(log10(labs[,nonNormal[14]]))
-labs[,nonNormal[14]] <- log10(labs[,nonNormal[14]])
-
-describe(labs[,nonNormal[15]])
-describe(log10(labs[,nonNormal[15]]))
-labs[,nonNormal[15]] <- log10(labs[,nonNormal[15]])
-
-describe(labs[,nonNormal[16]])
-describe(log10(labs[,nonNormal[16]]))
-labs[,nonNormal[16]] <- log10(labs[,nonNormal[16]])
-
-describe(labs[,nonNormal[17]])
-describe(sqrt(
-  sum(max(labs[,nonNormal[17]],na.rm=TRUE)+1)-labs[,nonNormal[17]]))
-labs[,nonNormal[17]] <- sqrt(
-  sum(max(labs[,nonNormal[17]],na.rm=TRUE)+1)-labs[,nonNormal[17]])
-
-describe(labs[,nonNormal[18]])
-describe(sqrt(
-  sum(max(labs[,nonNormal[18]],na.rm=TRUE)+1)-labs[,nonNormal[18]]))
-labs[,nonNormal[18]] <- sqrt(
-  sum(max(labs[,nonNormal[18]],na.rm=TRUE)+1)-labs[,nonNormal[18]])
-
-describe(labs[,nonNormal[19]])
-describe(sqrt(labs[,nonNormal[19]]))
-labs[,nonNormal[19]] <- sqrt(labs[,nonNormal[19]])
-
-describe(labs[,nonNormal[20]])
-describe(sqrt(labs[,nonNormal[20]]))
-labs[,nonNormal[20]] <- sqrt(labs[,nonNormal[20]])
-
-describe(labs[,nonNormal[21]])
-describe(log10(labs[,nonNormal[21]]))
-labs[,nonNormal[21]] <- log10(labs[,nonNormal[21]])
-
-describe(labs[,nonNormal[22]])
-describe(log10(labs[,nonNormal[22]]))
-labs[,nonNormal[22]] <- log10(labs[,nonNormal[22]])
-
-describe(labs[,nonNormal[23]])
-describe(sqrt(labs[,nonNormal[23]]))
-labs[,nonNormal[23]] <- sqrt(labs[,nonNormal[23]])
-
-describe(labs[,nonNormal[24]])
-describe(log10(labs[,nonNormal[24]]))
-labs[,nonNormal[24]] <- log10(labs[,nonNormal[24]])
-
-describe(labs[,nonNormal[25]])
-describe(log10(labs[,nonNormal[25]]))
-labs[,nonNormal[25]] <- log10(labs[,nonNormal[25]])
-
-describe(labs[,nonNormal[26]])
-describe(log10(labs[,nonNormal[26]]))
-labs[,nonNormal[26]] <- log10(labs[,nonNormal[26]])
-
-describe(labs[,nonNormal[27]])
-describe(log10(labs[,nonNormal[27]]))
-labs[,nonNormal[27]] <- log10(labs[,nonNormal[27]])
-
-describe(labs[,nonNormal[28]])
-describe(log10(labs[,nonNormal[28]]))
-labs[,nonNormal[28]] <- log10(labs[,nonNormal[28]])
-
-describe(labs[,nonNormal[29]])
-describe(log10(labs[,nonNormal[29]]))
-labs[,nonNormal[29]] <- log10(labs[,nonNormal[29]])
 
 #### PREP WEARABLE DATA ####
 
@@ -376,93 +240,3 @@ ID_Date <- corDf[,c("iPOP_ID","Clin_Result_Date")]
 corDf <- corDf[,-c(which(
   names(corDf) %in% c("iPOP_ID","Clin_Result_Date")))]
 
-#### RUN CORRELATIONS ####
-
-#Run
-vitalVars <- which(names(corDf) %in% c("Pulse","Temp"))
-wearVars <- which(names(corDf) %in% c(allWear))
-clinVars <- c(1:NCOL(corDf[-c(vitalVars,wearVars)]))
-
-results <- corr.test(corDf[,c(clinVars)],
-                     corDf[,c(vitalVars,wearVars)],
-                     method="pearson",adjust="fdr")
-
-#### VIEW CORRELATION RESULTS ####
-
-#View correlations
-results$r
-
-#View p-values
-results$p
-
-#View sample size for each test
-results$n
-
-#### CREATE DATA FRAME(S) SUMMARIZING RESULTS ####
-rCols <- paste0(dimnames(results$r)[[2]],"_R")
-pCols <- paste0(dimnames(results$p)[[2]],"_P")
-nCols <- paste0(dimnames(results$n)[[2]],"_N")
-
-allCors_R <- data.frame(placeHolder=rep(NA,length(allClin)))
-rownames(allCors_R) <- dimnames(results$r)[[1]]
-for(i in 1:length(rCols)){
-  allCors_R[,i] <- results$r[,i]
-  names(allCors_R)[i] <- rCols[i]
-}
-
-allCors_P <- data.frame(placeHolder=rep(NA,length(allClin)))
-rownames(allCors_P) <- dimnames(results$p)[[1]]
-for(i in 1:length(pCols)){
-  allCors_P[,i] <- results$p[,i]
-  names(allCors_P)[i] <- pCols[i]
-}
-
-allCors_N <- data.frame(placeHolder=rep(NA,length(allClin)))
-rownames(allCors_N) <- dimnames(results$n)[[1]]
-for(i in 1:length(nCols)){
-  allCors_N[,i] <- results$n[,i]
-  names(allCors_N)[i] <- nCols[i]
-}
-
-allCors <- cbind(allCors_R,allCors_P,allCors_N)
-
-newColOrder <- c("Pulse","Temp",allWear)
-
-tmp <- allCors
-for(i in 1:length(newColOrder)){
-  if(i == 1){
-    cache <- grep(newColOrder[i],names(allCors))
-    tmp[,c(i:sum(0+3))] <- allCors[,c(cache)]
-    names(tmp)[c(i:sum(0+3))] <- c(names(allCors)[c(cache)])
-    count <- sum(0+3)
-  } else {
-    if(i != 1){
-      cache <- grep(paste0("^",newColOrder[i]),names(allCors))
-      tmp[,c(sum(count+1):sum(count+3))] <- allCors[,c(cache)]
-      names(tmp)[c(sum(count+1):sum(count+3))] <- c(names(allCors)[c(cache)])
-      count <- sum(count+3)
-    }
-  }
-}
-
-allCors <- tmp
-sigCors_Ranked <- allCors
-
-#class(sigCors_Ranked$Pulse_P)
-sigCors_Ranked <- sigCors_Ranked[
-  which(sigCors_Ranked$Pulse_P < 0.05),]
-
-sigCors_Ranked <- sigCors_Ranked[
-  order(abs(sigCors_Ranked$Pulse_R),decreasing = TRUE),]
-
-#### SAVE DATA FRAME(S) SUMMARIZING RESULTS ####
-
-write.csv(allCors,
-          "~/Desktop/20170810_allCors_VitalsVsLabs.csv",
-          row.names=TRUE)
-
-write.csv(sigCors_Ranked,
-          "~/Desktop/20170810_sigCorsRanked_VitalsVsLabs.csv",
-          row.names=TRUE)
-
-#### END ####
