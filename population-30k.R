5dfcca597abc5b8486d158f814b5c221355e0fe2
Jessilyn <jessilyn@stanford.edu>
2017-12-19 19:30:03 -0800

    Prints ranked_models.csv from 30k data for input into the population_models.R scriptU

--- population-30k.R
+++ population-30k.R
@@ -9,14 +9,14 @@ labs = labs[,-c(1,2)]
 labs <- subset(labs, select=-c(ALCRU))
 #labs = as.numeric(labs)
 
-nms = names(labs)
+nms = names(subset(labs, select=-c(Pulse, Temp)))
 nms[1:(length(nms) - 2)] # list with test names
 
 n = nrow(labs) # total num of observations
 test = sample(n)[1:floor(n*0.1)] # 10% for testing
 
 ## Cross validated correlation
-# TODO: Some problem with the labs table
+thirtyk.lm= c()
 for (nm in nms){
   # prepare data for LM
   df = data.frame(labtest = labs[[nm]], Pulse = labs$Pulse, Temp = labs$Temp)
@@ -27,5 +27,10 @@ for (nm in nms){
   # predict
   pred = predict(model, newdata = df[test,])
   
-  print(paste(nm,cor(pred, labs[[nm]][test], use = "complete.obs")))
+  cor.coef <- cor(pred, labs[[nm]][test], use = "complete.obs")
+  print(paste(nm,cor.coef))
+  
+  thirtyk.lm= rbind(thirtyk.lm, c(nm,cor.coef))
 }
+
+write.table(thirtyk.lm, "../SECURE_data/ranked_models.csv",row.names=FALSE,col.names=FALSE, sep=",")
