154534af40a15b2c336c85aed22ef9707cef00c5
Łukasz Kidziński <lukasz.kidzinski@gmail.com>
2019-04-15 15:47:42 -0700

    Short script for generating Fig 2d based on the summary table

---
+++ scripts/extra-plotting/fig2d.R
@@ -0,0 +1,25 @@
+library(ggplot2)
+library(dplyr)
+
+weartals_theme = theme_classic() + theme(text = element_text(size=18), panel.border = element_blank(), axis.text.x = element_text(angle = 45, hjust = 1))
+
+# save(all.res,file="all.res.Rda")
+load("all.res.Rda")
+
+grouped <- group_by(all.res, model, test)
+
+df = grouped %>%
+  summarise(mean=mean(rve), sd=sd(rve))  %>%
+  arrange(model,desc(mean))
+df$test = factor(df$test, levels = unique(df$test))
+
+df$min = df$mean - df$sd
+df$min[df$min < 0] = 0
+df$max = df$mean + df$sd
+p = ggplot(df, mapping= aes(x=test,y=mean,color=model)) + 
+  geom_errorbar(aes(ymin=min, ymax=max), width=.2) +
+  geom_point(size=3) +
+  weartals_theme + 
+  labs(x = NULL, y ="RPVE")
+p
+ggsave(paste0("population.png"),p,width=14,height=5)
