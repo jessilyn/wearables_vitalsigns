ef8800643a1b3b387dc4bea03794dcd5ecc985e1
Lukasz Kidzinski <lukasz.kidzinski@gmail.com>
2019-05-15 20:26:09 -0700

    RF vs personal mean

--- scripts/extra-plotting/fig4b.R
+++ scripts/extra-plotting/fig4b.R
@@ -5,14 +5,34 @@ source("ggplot-theme.R") # just to make things look nice
 
 load("figure4b.Rda")
 
-# Some preprocessing
-toplot.30k = group_by(res.30k,test,model) %>%
-  summarise(mean = mean(value), sd = sd(value), pval = mean(value<0) )
+#####
+# Figure: iPOP RF vs personal mean
 res = res.iPOP[,c(1,7,9)]
 colnames(res)[2] = "personal mean (iPOP)"
+
 toplot.iPOP = gather(res, model, value, -test) %>%
   group_by(test, model) %>%
   summarise(mean = mean(value), sd = sd(value), pval = mean(value<0))
+
+toplot.iPOP = toplot.iPOP[order(-toplot.iPOP$mean),]
+toplot.iPOP = toplot.iPOP[order(toplot.iPOP$model,decreasing = TRUE),] # order by the personal mean
+toplot.iPOP$test = factor(as.character(toplot.iPOP$test), levels = unique(as.character(toplot.iPOP$test)))
+
+pp = ggplot(toplot.iPOP, aes(test, mean, group = model, color = model)) +
+  ylab(expression(sqrt("Variance explained"))) +
+  xlab("Lab test") +
+  geom_point(size = 3, position=position_dodge(width=0.7)) +
+  geom_errorbar(size = 0.8, aes(ymin=mean-sd, ymax=mean+sd), width=.8, position=position_dodge(width=0.7)) +
+  weartals_theme + 
+  theme(text = element_text(size=14))
+
+print(pp)
+
+#####
+# Figure: iPOP vs 30K
+# Some preprocessing
+toplot.30k = group_by(res.30k,test,model) %>%
+  summarise(mean = mean(value), sd = sd(value), pval = mean(value<0) )
 toplot = rbind(toplot.30k,toplot.iPOP)
 
 # Order by 
